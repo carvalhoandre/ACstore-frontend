88460d76f093c272f7a0b0b1e51489cde3662159
André Carvalho <andre_carvalho0@live.com>
2020-12-30 21:22:45 -0300

    Tela de confirmação de pedido

---
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CartItem } from '../../models/cart-item';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { EnderecoDTO } from '../../models/endereco.dto';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
+import { ClienteService } from '../../services/domain/cliente.service';
+
+@IonicPage()
+@Component({
+  selector: 'page-order-confirmation',
+  templateUrl: 'order-confirmation.html',
+})
+export class OrderConfirmationPage {
+
+  pedidio: PedidoDTO;
+  cartItems: CartItem[];
+  cliente : ClienteDTO;
+  endereco: EnderecoDTO;
+  
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    public cartService: CartService,
+    public clienteService: ClienteService) {
+
+    this.pedidio = navParams.get('pedido');
+  }
+
+  ionViewDidLoad() {
+    this.cartItems = this.cartService.getCart().items;
+    
+    this.clienteService.findById(this.pedidio.cliente.id)
+      .subscribe(response => {
+        this.cliente = response as ClienteDTO;
+        this.endereco = this.findEndereco(this.pedidio.enderecoDeEntrega.id, response['enderecos']);
+      },
+      error => {
+        this.navCtrl.setRoot('HomePage');
+      });
+  }
+
+
+  private findEndereco(id: string, list: EnderecoDTO[]) : EnderecoDTO {
+    let position = list.findIndex(x => x.id == id);
+    return list[position];
+  }
+
+  total() {
+    return this.cartService.total();
+  }
+}
