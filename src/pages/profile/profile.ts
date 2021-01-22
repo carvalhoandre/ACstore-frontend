48620768c74cd872707262ed17bc9d4251e1af1f
André Carvalho <andre_carvalho0@live.com>
2021-01-22 19:37:13 -0300

    :bug: camera correction export

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -116,8 +116,9 @@ export class ProfilePage {
       const options: CameraOptions = {
         quality: 100,
         destinationType: this.camera.DestinationType.DATA_URL,
-        encodingType: this.camera.EncodingType.PNG,
-        mediaType: this.camera.MediaType.PICTURE
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE,
+        correctOrientation: true
       }
 
       this.camera.getPicture(options).then((imageData) => {
